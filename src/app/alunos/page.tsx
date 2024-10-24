1bdb7d9017c4a7dd9fb2a8b8d5e5463aa976f551
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-24 16:50:50 -0300

    adicionando grafico da media das notas de cada atividade na main

--- src/app/alunos/page.tsx
+++ src/app/alunos/page.tsx
@@ -1,9 +1,46 @@
 "use client";
 
 import { TipoAluno } from "@/types";
-import Link from "next/link";
+import { color } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react"
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,   
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  
+  const dadosGraficoPorAluno = [
+    {
+      dados: {
+        labels: ["Checkpoint 1", "Checkpoint 2", "Challenge", "Global Solution"],
+        datasets: [
+          {
+            label: "Notas",
+            data: [85, 90, 88, 92], 
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          },
+        ],
+      },
+    },
+  ];
 
 export default function Alunos() {
 
@@ -17,40 +54,102 @@ export default function Alunos() {
         }
         chamadaApi();
     },[]);
-    
-   
+    const responsividadeGrafico = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            labels: {
+              color: '#ff0000',  
+              font: {
+                size: 14,  
+              },
+            },
+          },
+          tooltip: {
+            titleFont: {
+              size: 16,
+            },
+            bodyFont: {
+              size: 14,
+              color: '#ff0000',  
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: '#ff0000', 
+            },
+          },
+          y: {
+            ticks: {
+              color: '#ff0000', 
+            },
+          },
+        },
+      };
+      
+      
+
+    const dadosGraficoPorAluno = Alunos.map((aluno) => ({
+        nome: aluno.nome,
+        dados: {
+            labels: ['Checkpoints', 'Challenges', 'Global Solutions'],
+            datasets: [
+                {
+                    label: aluno.nome,
+                    data: [
+                        aluno.notasCp.reduce((a, b) => a + b, 0) / aluno.notasCp.length || 0,
+                        aluno.notasChallenge.reduce((a, b) => a + b, 0) / aluno.notasChallenge.length || 0,
+                        aluno.notasGlobal.reduce((a, b) => a + b, 0) / aluno.notasGlobal.length || 0,
+                    ],
+                    borderColor: '#ff0000',
+                    backgroundColor: '#ffffff',
+                    
+                },
+            ],
+        }
+    }));
+
     return (
-        <div className="flex-col m-5 items-center justify-center">
-            <h2 className="font-bold text-5xl pb-20">Alunos</h2>
+        <div className="flex-col m-5 items-center justify-center ">
+            <h2 className="flex-col font-bold text-5xl pb-20 text-center">Alunos</h2>
             <section className="sessao-aluno flex flex-wrap items-center justify-center gap-5">
-                {Alunos.map((p) => (
-                        <div key={p.id} className=" bg-red-900 w-full sm:w-1/2 md:w-1/3 h-[600px] lg:h-[500px] p-6 rounded-md flex flex-col justify-between ">
-                            <h2 className="nome-aluno text-3xl"> {p.nome}</h2>
-                            <h2 className="rm-aluno text-2xl">RM:{p.rm}</h2>
+                {Alunos.map((a,index) => (
+                        <div key={a.id} className=" bg-red-900 w-full sm:w-1/1 md:w-1/3 h-[1000px] lg:h-[900px] p-6 rounded-md flex flex-col justify-between ">
+                            <h2 className="nome-aluno text-3xl"> {a.nome}</h2>
+                            <h2 className="rm-aluno text-2xl">RM:{a.rm}</h2>
                             <div className="flex flex-row  pt-3 pb-3">
                                 <figure className="w-1/2">
-                                    <Image src={p.imagem} className="imagem-aluno rounded-lg" alt="imagem aluno" width={250} height={300} />
+                                    <Image src={a.imagem} className="imagem-aluno rounded-lg" alt="imagem aluno" width={250} height={300} />
                                 </figure>
                                 
                                 <div className="flex flex-col ml-4"> 
                                     <h3>Media das notas dos checkpoints:
-                                        <p className="font-bold">{(p.notasCp.reduce((a, b) => a + b, 0) / p.notasCp.length || 0).toFixed(2)}</p>
+                                        <p className="font-bold">{(a.notasCp.reduce((a, b) => a + b, 0) / a.notasCp.length || 0).toFixed(2)}</p>
                                     </h3>
                                     <h3>Media das notas do challenge:
-                                        <p className="font-bold">{(p.notasChallenge.reduce((a, b) => a + b, 0) / p.notasChallenge.length || 0).toFixed(2)}</p>
+                                        <p className="font-bold">{(a.notasChallenge.reduce((a, b) => a + b, 0) / a.notasChallenge.length || 0).toFixed(2)}</p>
                                     </h3>
                                     <h3>Media das notas das global solution:
-                                        <p className="font-bold mb-6">{(p.notasGlobal.reduce((a, b) => a + b, 0) / p.notasGlobal.length || 0).toFixed(2)}</p>
+                                        <p className="font-bold mb-6">{(a.notasGlobal.reduce((a, b) => a + b, 0) / a.notasGlobal.length || 0).toFixed(2)}</p>
                                     </h3>
                                 </div>
                             </div>
+                            <div className="w-[300px] h-[300px]">
+                                 <Line data={dadosGraficoPorAluno[index].dados} options={responsividadeGrafico}/>
+                            </div>
+                           
+                            
                             <div className="bg-red-500 rounded-lg w-full py-2 mb-6 text-center hover:bg-red-700 duration-300 hover:shadow-lg  cursor-pointer">
                                     <button type="submit">Acessar Perfil</button> 
                             </div>
                             
                          </div>
                     ))}
-
+                     
+                         
 
             </section>
            
--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 export default function Cabecalho(){
     return(
         <header className="flex items-center justify-evenly ">
-            <Image src={imgFiap} alt="logo fiap" width={350} height={250}></Image>
+            <Image src={imgFiap} alt="logo fiap" width={150} height={250} className="mt-2"></Image>
             <ul className="menu-header flex items-center justify-evenly gap-10">
-                <li className="links"><Link href="/">Pagina Inicial</Link></li>
-                <li className="links"><Link href="/">Alterar nota</Link></li>
-                <li className="links"><Link href="/">Cadastrar nota</Link></li>
+                <li className="links s"><Link href="/">Pagina Inicial</Link></li>
+                <li className="links"><a href="https://github.com/ViniciusLABernardes/CP6-Front" target="blank">Github</a></li>
             </ul>
         </header>
     )
