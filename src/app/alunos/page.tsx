b73c4c5ece0001919d25ba936c4b5e620ac06518
David-Rapeckman <Rapeckmandavid@gmail.com>
2024-10-27 19:20:31 -0300

    Corrigir erro de sintaxe no componente Alunos
    Co-authored-by: gugomesx10 <gustavogomesmartins4@icloud.com>
    Co-authored-by: Brendon-Brasil <brendonbrasil799@gmail.com>

--- src/app/alunos/page.tsx
+++ src/app/alunos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React from 'react';
 import { TipoAluno } from "@/types";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -63,7 +63,7 @@ export default function Alunos() {
     <div className="flex-col m-5 items-center justify-center">
       <h2 className="flex-col font-bold text-5xl pb-20 text-center text-white">Alunos</h2>
       <section className="sessao-aluno flex flex-wrap items-center justify-center gap-5">
-        {Alunos.map((a, index) => (
+        {Alunos.map((a) => (
           <div key={a.id} className="bg-red-900 w-full sm:w-1/1 md:w-1/3 h-[700px] lg:h-[600px] p-6 rounded-md flex flex-col justify-between">
             <h2 className="nome-aluno text-3xl text-white"> {a.nome}</h2>
             <h2 className="rm-aluno text-2xl text-white">RM: {a.rm}</h2>
