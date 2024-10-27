42e2404989ad34f24d91fbd20ac9847707c13336
David-Rapeckman <Rapeckmandavid@gmail.com>
2024-10-27 20:25:56 -0300

    Correção Exibir botão de editar ao passar o mouse

--- src/app/alunos/[id]/page.tsx
+++ src/app/alunos/[id]/page.tsx
@@ -97,126 +97,145 @@ export default function AtualizarNota() {
     ],
   };
 
-  return (
-    <div className="flex flex-row p-5 bg-[#1e1e1e] min-h-screen text-white space-x-6">
-      {aluno ? (
-        <>
-          <div className="flex flex-col w-1/3 space-y-6">
-            <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-pink-400">Checkpoints</h3>
-              <div className="flex space-x-2 mt-3">
-                {aluno.notasCp.map((nota, index) => (
-                  <div key={index} className="p-3 bg-gray-700 rounded-md text-center w-16 relative">
-                    {nota}
-                    <button
-                      onClick={() => {
-                        setAlterarNota({ index, tipo: "Cp", nota });
-                        setShowModal(true);
-                      }}
-                      className="absolute top-0 right-0 text-xs text-yellow-300"
+    return (
+      <div className="flex flex-row p-5 bg-[#1e1e1e] min-h-screen text-white space-x-6">
+        {aluno ? (
+          <>
+            <div className="flex flex-col w-1/3 space-y-6">
+              <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
+                <h3 className="text-2xl font-semibold text-pink-400">Checkpoints</h3>
+                <div className="flex space-x-2 mt-3">
+                  {aluno.notasCp.map((nota, index) => (
+                    <div
+                      key={index}
+                      className="p-3 bg-gray-700 rounded-md text-center w-16 relative"
+                      onMouseEnter={() => setAlterarNota({ index, tipo: "Cp", nota })}
+                      onMouseLeave={() => setAlterarNota({ index: -1, tipo: "", nota: 0 })}
                     >
-                      ✏️
-                    </button>
-                  </div>
-                ))}
-              </div>
-            </section>
-            <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-yellow-400">Sprints</h3>
-              <div className="flex space-x-2 mt-3">
-                {aluno.notasChallenge.map((nota, index) => (
-                  <div key={index} className="p-3 bg-gray-700 rounded-md text-center w-16 relative">
-                    {nota}
-                    <button
-                      onClick={() => {
-                        setAlterarNota({ index, tipo: "Challenge", nota });
-                        setShowModal(true);
-                      }}
-                      className="absolute top-0 right-0 text-xs text-yellow-300"
+                      {nota}
+                      {alterarNota.index === index && (
+                        <button
+                          onClick={() => {
+                            setShowModal(true);
+                          }}
+                          className="absolute top-0 right-0 text-xs text-yellow-300"
+                        >
+                          ✏️
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </section>
+              <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
+                <h3 className="text-2xl font-semibold text-yellow-400">Sprints</h3>
+                <div className="flex space-x-2 mt-3">
+                  {aluno.notasChallenge.map((nota, index) => (
+                    <div
+                      key={index}
+                      className="p-3 bg-gray-700 rounded-md text-center w-16 relative"
+                      onMouseEnter={() => setAlterarNota({ index, tipo: "Challenge", nota })}
+                      onMouseLeave={() => setAlterarNota({ index: -1, tipo: "", nota: 0 })}
                     >
-                      ✏️
-                    </button>
-                  </div>
-                ))}
-              </div>
-            </section>
-            <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-green-400">Global Solutions</h3>
-              <div className="flex space-x-2 mt-3">
-                {aluno.notasGlobal.map((nota, index) => (
-                  <div key={index} className="p-3 bg-gray-700 rounded-md text-center w-16 relative">
-                    {nota}
-                    <button
-                      onClick={() => {
-                        setAlterarNota({ index, tipo: "Global", nota });
-                        setShowModal(true);
-                      }}
-                      className="absolute top-0 right-0 text-xs text-yellow-300"
+                      {nota}
+                      {alterarNota.index === index && (
+                        <button
+                          onClick={() => {
+                            setShowModal(true);
+                          }}
+                          className="absolute top-0 right-0 text-xs text-yellow-300"
+                        >
+                          ✏️
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </section>
+              <section className="p-5 bg-gray-800 rounded-lg shadow-lg">
+                <h3 className="text-2xl font-semibold text-green-400">Global Solutions</h3>
+                <div className="flex space-x-2 mt-3">
+                  {aluno.notasGlobal.map((nota, index) => (
+                    <div
+                      key={index}
+                      className="p-3 bg-gray-700 rounded-md text-center w-16 relative"
+                      onMouseEnter={() => setAlterarNota({ index, tipo: "Global", nota })}
+                      onMouseLeave={() => setAlterarNota({ index: -1, tipo: "", nota: 0 })}
                     >
-                      ✏️
-                    </button>
-                  </div>
-                ))}
-              </div>
-            </section>
-          </div>
-
-          <div className="flex flex-col w-1/3 items-center justify-top">
-            <h2 className="text-3xl font-bold text-white mb-4">{aluno?.nome}</h2>
-            <h3 className="text-2xl font-semibold text-yellow-400 mb-4">Gráfico de Notas</h3>
-            <div className="bg-gray-800 w-full">
-              <Bar data={data} options={{ responsive: true, plugins: { legend: { display: false } } }} />
+                      {nota}
+                      {alterarNota.index === index && (
+                        <button
+                          onClick={() => {
+                            setShowModal(true);
+                          }}
+                          className="absolute top-0 right-0 text-xs text-yellow-300"
+                        >
+                          ✏️
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </section>
             </div>
-          </div>
-
-          <div className="flex flex-col w-1/3 space-y-6">
-            <form onSubmit={handleAddNota} className="flex flex-col bg-gray-800 p-5 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold text-purple-400 mb-4">Adicionar Nota</h3>
-              <input
-                type="text"
-                value={novaNota.nomeAtividade}
-                onChange={(e) => setNovaNota({ ...novaNota, nomeAtividade: e.target.value })}
-                required
-                className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md"
-                placeholder="Nome da Atividade"
-              />
-              <input
-                type="number"
-                value={novaNota.nota}
-                onChange={(e) => setNovaNota({ ...novaNota, nota: parseFloat(e.target.value) })}
-                required
-                className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md"
-                placeholder="Nota"
-              />
-              <button type="submit" className="bg-purple-600 text-white font-bold py-2 px-4 rounded-md hover:bg-purple-500 mt-3">
-                Adicionar Nota
-              </button>
-            </form>
-          </div>
-
-          {showModal && (
-            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-              <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-                <h3 className="text-xl font-semibold text-yellow-400 mb-4">Alterar Nota</h3>
+    
+            <div className="flex flex-col w-1/3 items-center justify-top">
+              <h2 className="text-3xl font-bold text-white mb-4">{aluno?.nome}</h2>
+              <h3 className="text-2xl font-semibold text-yellow-400 mb-4">Gráfico de Notas</h3>
+              <div className="bg-gray-800 w-full">
+                <Bar data={data} options={{ responsive: true, plugins: { legend: { display: false } } }} />
+              </div>
+            </div>
+    
+            <div className="flex flex-col w-1/3 space-y-6">
+              <form onSubmit={handleAddNota} className="flex flex-col bg-gray-800 p-5 rounded-lg shadow-lg">
+                <h3 className="text-2xl font-semibold text-purple-400 mb-4">Adicionar Nota</h3>
+                <input
+                  type="text"
+                  value={novaNota.nomeAtividade}
+                  onChange={(e) => setNovaNota({ ...novaNota, nomeAtividade: e.target.value })}
+                  required
+                  className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md"
+                  placeholder="Nome da Atividade"
+                />
                 <input
                   type="number"
-                  value={alterarNota.nota}
-                  onChange={(e) => setAlterarNota({ ...alterarNota, nota: parseFloat(e.target.value) })}
-                  className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md mb-4"
+                  value={novaNota.nota}
+                  onChange={(e) => setNovaNota({ ...novaNota, nota: parseFloat(e.target.value) })}
+                  required
+                  className="mt-1 block w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md"
+                  placeholder="Nota"
                 />
-                <button onClick={handleEditNota} className="bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-500">
-                  Confirmar
+                <button type="submit" className="bg-purple-600 text-white font-bold py-2 px-4 rounded-md hover:bg-purple-500 mt-3">
+                  Adicionar Nota
                 </button>
-                <button onClick={() => setShowModal(false)} className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-500 ml-2">
-                  Cancelar
-                </button>
-              </div>
+              </form>
             </div>
-          )}
-        </>
-      ) : (
-        <p>Carregando aluno...</p>
-      )}
-    </div>
-  );
+    
+            {showModal && (
+              <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+                  <h3 className="text-xl font-semibold text-yellow-400 mb-4">Alterar Nota</h3>
+                  <input
+                    type="number"
+                    value={alterarNota.nota}
+                    onChange={(e) => setAlterarNota({ ...alterarNota, nota: parseFloat(e.target.value) })}
+                    className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded-md mb-4"
+                  />
+                  <button onClick={handleEditNota} className="bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-500">
+                    Confirmar
+                  </button>
+                  <button onClick={() => setShowModal(false)} className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-500 ml-2">
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            )}
+          </>
+        ) : (
+          <p>Carregando aluno...</p>
+        )}
+      </div>
+    );
+    
 }
