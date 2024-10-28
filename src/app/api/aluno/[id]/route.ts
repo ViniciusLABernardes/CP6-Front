7b5f99f6ecbed76c2c5de59903f886c6659db908
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-27 22:24:03 -0300

    arrumando put, mudando de number para string

--- src/app/api/aluno/[id]/route.ts
+++ src/app/api/aluno/[id]/route.ts
@@ -12,26 +12,35 @@ export async function GET(request: Request, { params }: { params: { id: string }
     return NextResponse.json(aluno);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: number } }) {
 
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
+    const alunos: TipoAluno[] = JSON.parse(file);
   
-    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const { nome, rm, imagem, notasCp, notasChallenge, notasGlobal, notaNova } = await request.json();
+  
+ 
+    const alunoId = parseInt(params.id, 10);
+    const indice = alunos.findIndex(a => a.id === alunoId);
+  
+    if (indice !== -1) {
 
-    const alunos:TipoAluno[] = JSON.parse(file);
-    
-    const{nome,rm,imagem,notasCp,notasChallenge,notasGlobal,notaNova} = await request.json();
-
-    const indice = alunos.findIndex(a => a.id == params.id);
-
-    if(indice != -1){
-        alunos.splice(indice, 1, { id: parseInt(params.id.toString()) , nome,rm, imagem, notasCp,notasChallenge,notasGlobal,notaNova});
-    
-       
-        const fileUpdate = JSON.stringify(alunos);
-        await fs.writeFile(process.cwd() + '/src/data/base.json',fileUpdate);
-            
-        return NextResponse.json({msg:"Nota atualizada com sucesso!"});
-    
+      alunos.splice(indice, 1, {
+        id: alunoId,
+        nome,
+        rm,
+        imagem,
+        notasCp,
+        notasChallenge,
+        notasGlobal,
+        notaNova
+      });
+  
+      const fileUpdate = JSON.stringify(alunos, null, 2);
+      await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+  
+      return NextResponse.json({ msg: "Nota atualizada com sucesso!" });
     }
-}
-
+  
+    return NextResponse.json({ msg: "Aluno não encontrado" }, { status: 404 });
+  }
