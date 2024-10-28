7a5f58a3762c1dc65f989de6f2f22b50d217d5ab
joaogananca26 <joaogananca26@gmail.com>
2024-10-27 21:12:11 -0300

    Metade da página de erro feita

--- src/components/Error/page.tsx
+++ src/components/Error/page.tsx
@@ -1,5 +1,16 @@
+import Image from 'next/image'
+
+
 export default function ErrorComponent(){
 return(
-    <></>
+    <div className="bg-white">
+        <h2>Esta página não existe, para voltar para página inicial, clique aqui!</h2>
+        <Image
+      src="/CP6-Front\src\img\stitch.jpg"
+      width={500}
+      height={500}
+      alt="Stitch triste"
+    />
+    </div>
 )
 }
